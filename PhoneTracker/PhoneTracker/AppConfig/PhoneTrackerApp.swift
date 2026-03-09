64e4896ff6bef509d89636176eeec1087174dc48
Игорь Николаев <igorek0706@gmail.com>
2026-03-09 18:45:05 +0300

    base project added

---
+++ PhoneTracker/PhoneTracker/AppConfig/PhoneTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneTrackerApp.swift
+//  PhoneTracker
+//
+//  Created by Игорь Николаев on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhoneTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
